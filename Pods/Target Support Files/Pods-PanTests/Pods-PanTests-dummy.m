dd0a93c5e38cd442b8d9524911bda50204e088f8
HongSeoYoung <swucomputer@SWUCOMPUTERui-MacBook-Air-2.local>
2017-05-21 23:25:49 +0900

    ㅇㅇ

---
+++ Pods/Target Support Files/Pods-PanTests/Pods-PanTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PanTests : NSObject
+@end
+@implementation PodsDummy_Pods_PanTests
+@end
---
+++ Pods/Target Support Files/Pods-PanUITests/Pods-PanUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PanUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PanUITests
+@end
